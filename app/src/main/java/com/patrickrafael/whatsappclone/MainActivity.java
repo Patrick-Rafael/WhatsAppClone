c1cf70d8079b45fe3eadbfb0bd68d2f458c3b0f9
Patrick <patrickrafael05@gmail.com>
2022-01-10 16:48:46 -0300

    Adicionando dependencias FireBase

---
+++ app/src/main/java/com/patrickrafael/whatsappclone/MainActivity.java
@@ -0,0 +1,16 @@
+package com.patrickrafael.whatsappclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
