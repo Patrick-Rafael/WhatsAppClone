412742309472b0c3784a2a3e9346ccd2a9b8f687
patrick Rafael <PatrickRafael05@gmail.com>
2022-02-04 17:01:55 -0300

    Adicionando Abas

--- app/src/main/java/com/activity/MainActivity.java
+++ app/src/main/java/com/activity/MainActivity.java
@@ -3,6 +3,7 @@ package com.activity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,13 +11,17 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.config.ConfigFireBase;
+import com.fragments.ContatosFragment;
+import com.fragments.ConversasFragment;
 import com.google.firebase.auth.FirebaseAuth;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
 import com.patrickrafael.whatsappclone.R;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth autenticacao;
-
 
 
     @Override
@@ -31,6 +36,22 @@ public class MainActivity extends AppCompatActivity {
         toolbar.setTitle("WhatsApp");
         setSupportActionBar(toolbar);
 
+        //Configurar abas
+
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+
+                getSupportFragmentManager(),
+                FragmentPagerItems.with(this)
+                        .add("Conversas", ConversasFragment.class)
+                        .add("Contatos", ContatosFragment.class)
+                        .create()
+
+        );
+        ViewPager viewPager = findViewById(R.id.viewPager);
+        viewPager.setAdapter(adapter);
+
+        SmartTabLayout viewPagerTab = findViewById(R.id.smartTabLayout);
+        viewPagerTab.setViewPager(viewPager);
 
 
     }
@@ -49,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
-        switch ( item.getItemId() ){
+        switch (item.getItemId()) {
 
             case R.id.menuSair:
 
@@ -57,19 +78,19 @@ public class MainActivity extends AppCompatActivity {
                 finish();
 
 
-            break;
+                break;
         }
 
         return super.onOptionsItemSelected(item);
 
     }
 
-    public void deslogarUsuario(){
+    public void deslogarUsuario() {
 
         try {
             autenticacao.signOut();
 
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
 
         }
