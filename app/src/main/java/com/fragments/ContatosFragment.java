2c69e0d65861fe71ced06b5fc482791244d388bc
patrick Rafael <PatrickRafael05@gmail.com>
2022-03-02 16:40:31 -0300

    Listando contatos

--- app/src/main/java/com/fragments/ContatosFragment.java
+++ app/src/main/java/com/fragments/ContatosFragment.java
@@ -3,42 +3,38 @@ package com.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.Model.Usuario;
+import com.adapter.ContatosAdapter;
 import com.patrickrafael.whatsappclone.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ContatosFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
+
 public class ContatosFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
+
     private String mParam1;
     private String mParam2;
 
+    private RecyclerView recyclerListaContatos;
+    private ContatosAdapter adapter;
+    private ArrayList<Usuario> listaContatos = new ArrayList<>();
+
     public ContatosFragment() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ContatosFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static ContatosFragment newInstance(String param1, String param2) {
         ContatosFragment fragment = new ContatosFragment();
         Bundle args = new Bundle();
@@ -61,6 +57,22 @@ public class ContatosFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contatos, container, false);
+        View view =  inflater.inflate(R.layout.fragment_contatos, container, false);
+
+        //Configurar Recycyler
+        recyclerListaContatos = view.findViewById(R.id.recyclerListaContatos);
+
+        //Adapter
+        adapter = new ContatosAdapter(listaContatos,getActivity());
+
+
+        //Recycler
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerListaContatos.setLayoutManager(layoutManager);
+        recyclerListaContatos.setHasFixedSize(true);
+        recyclerListaContatos.setAdapter(adapter);
+
+
+        return view;
     }
 }
