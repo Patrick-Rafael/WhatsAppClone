24ff3b86a68a32a3102a6a6b95e3c76ef7da81f7
patrick Rafael <PatrickRafael05@gmail.com>
2022-03-17 15:13:09 -0300

    Salvando mensagens

--- app/src/main/java/com/activity/ChatActivity.java
+++ app/src/main/java/com/activity/ChatActivity.java
@@ -5,10 +5,20 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.service.controls.actions.FloatAction;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.Model.Mensagem;
 import com.Model.Usuario;
 import com.bumptech.glide.Glide;
+import com.config.ConfigFireBase;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DatabaseReference;
+import com.helper.Base64Custom;
+import com.helper.UsuarioFireBase;
 import com.patrickrafael.whatsappclone.R;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -18,6 +28,9 @@ public class ChatActivity extends AppCompatActivity {
     private TextView textViewNome;
     private CircleImageView circleImageViewFoto;
     private Usuario usuarioDestinatario;
+    private FloatingActionButton buttonMenssagem;
+    private EditText editMensagem;
+    private String idUsuarioRementente, idUsuarioDestinatario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,26 +42,77 @@ public class ChatActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         //Config inicial
-        textViewNome = findViewById(R.id.textViewNomeChat);
-        circleImageViewFoto = findViewById(R.id.circleImageViewChat);
+        ids();
+
+        //Recupera dados do usuario remetente
+        idUsuarioRementente = UsuarioFireBase.getIdentificadorUsuario();
 
         //Pega dados do usuario destinatario
         Bundle bundle = getIntent().getExtras();
-        if(bundle != null ){
+        if (bundle != null) {
 
             usuarioDestinatario = (Usuario) bundle.getSerializable("chatContato");
             textViewNome.setText(usuarioDestinatario.getNome());
             //Recuperar foto
             String foto = usuarioDestinatario.getFoto();
-            if(foto != null){
+            if (foto != null) {
                 Uri url = Uri.parse(usuarioDestinatario.getFoto());
                 Glide.with(ChatActivity.this).load(url).into(circleImageViewFoto);
-            }else {
+            } else {
                 circleImageViewFoto.setImageResource(R.drawable.padrao);
             }
 
+            //Recuperar dados usuario destinatario
+            idUsuarioDestinatario = Base64Custom.codificarBase64(usuarioDestinatario.getEmail());
+
         }
 
+
+    }
+
+    public void enviarMensagem(View view) {
+
+        String textoMensagem = editMensagem.getText().toString();
+        if (!textoMensagem.isEmpty()) {
+
+
+            Mensagem mensagem = new Mensagem();
+            mensagem.setIdUsuario(idUsuarioRementente);
+            mensagem.setMensagem(textoMensagem);
+
+            //Salvar Mensagem para o Remetente
+            salvarMensagem(idUsuarioRementente, idUsuarioDestinatario, mensagem);
+
+        } else {
+            Toast.makeText(ChatActivity.this, "Digite uma mensagem para enviar!", Toast.LENGTH_LONG).show();
+
+        }
+
+    }
+
+    private void salvarMensagem(String idRementente, String idDestinatario, Mensagem mensagem) {
+
+        DatabaseReference database = ConfigFireBase.getFireBaseDataBase();
+        DatabaseReference mensagemRef = database.child("mensagens");
+
+        mensagemRef.child(idRementente)
+                .child(idDestinatario)
+                .push()
+                .setValue(mensagem);
+
+        //Limpar texto
+
+        editMensagem.setText("");
+
+
+    }
+
+    public void ids() {
+
+        textViewNome = findViewById(R.id.textViewNomeChat);
+        circleImageViewFoto = findViewById(R.id.circleImageViewChat);
+        buttonMenssagem = findViewById(R.id.fabEnviar);
+        editMensagem = findViewById(R.id.editMensagem);
 
     }
 }
